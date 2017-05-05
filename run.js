b8dbcdad996344f55e9bbefbe93d26c8877709bf
Harshvardhan Malpani <harsh.malpani25@gmail.com>
2017-05-05 17:58:02 +0530

    useless lines removed

--- run.js
+++ run.js
@@ -1,18 +1,4 @@
-/*
-To obtain access token, go to https://developers.facebook.com/tools/explorer/
-Press GET TOKEN -> GET USER ACCESS TOKEN
-Check *** user_likes *** (second col second row probably), click get access token. Copy and paste it in the script line #22
-
-open https://mbasic.facebook.com
-Press CTRL + SHIFT + J
-copy and paste all of this and hit enter...
-Press one more button - self explanatory
-
-Libraries used - jquery
-Auth: Harshvardhan Malpani - https://harshmalpani.in
-https://www.facebook.com/harshvardhanmalpani
-*/
-
+// Use read.md for documentation and how to
 var access_token="<<< YOUR ACCESS TOKEN >>>";
 var limit_quota="100";
 
